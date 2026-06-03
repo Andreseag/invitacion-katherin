8f1575e38aed0ec710b53bbbbf47aa81ec785534
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-06-03 14:13:00 -0500

    fix: fix error in astro config :bug:

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://invitacion-katherin.netlify.app/",
+  site: "https://invitacion-katherin.netlify.app",
   vite: {
     plugins: [tailwindcss()],
   },
